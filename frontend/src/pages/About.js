6437f0e89ab9982441d8a8b8c76131dcdeb46f69
taviishii <tavishi.9905@gmail.com>
2025-06-06 23:00:56 +0400

    extra changes

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import github from "../assets/github.png";
+// import github from "../assets/github.png";
 import patternBkg from "../assets/gym-pattern.png";
 
 export default function About() {
@@ -28,9 +28,6 @@ export default function About() {
           shouldn't expect to be able to access your account a month or a week
           after you created it.`}
         </p>
-        <a href="https://github.com/Marija-Kov/Workout-Mate">
-          <img className="github" src={github} alt="Github" />
-        </a>
       </div>
       <img
         className="about--gym--pattern--bkg"
